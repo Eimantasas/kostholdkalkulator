5c8ee79965221ef462d78af8389fd978e1ed5ba0
Eimantasas <152972605+Eimantasas@users.noreply.github.com>
2024-04-04 12:59:11 +0200

    Add files via upload

---
+++ Python kosthold app kalkulator/main.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from tkinter import ttk
+
+
+class App():
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.geometry("200x300")
+        self.root.mainloop()
+        return
+
+if __name__ == "__main__":
+    App()
