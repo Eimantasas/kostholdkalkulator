028486999178ce323e62216c199480e45aeda568
OmerEryigit7 <152978422+OmerEryigit7@users.noreply.github.com>
2024-04-05 15:00:10 +0200

    Update main.py
    Text.py

--- Python kosthold app kalkulator/main.py
+++ Python kosthold app kalkulator/main.py
@@ -1,14 +1,29 @@
-import tkinter as tk
+from tkinter import *
 from tkinter import ttk
 
+ 
+window = Tk()
+window.geometry("420x420")
 
-class App():
-    def __init__(self):
-        self.root = tk.Tk()
-        self.root.geometry("200x300")
-        self.root.mainloop()
-        return
-        print("baller")
+label = Label(window, text="asdfasdf")
+label.place(x=210, y=210)
 
-if __name__ == "__main__":
-    App()
+inputBox_food = ttk.Combobox(window, values= ["Egg", "Storfekjøtt", "Melk", "Kikerter"])
+inputBox_food.place(x=1, y=2)
+
+inputBox_amount = ttk.Combobox(window, values= ["Per 100 gram", "1 stykke"])
+inputBox_amount.place(x=1, y=50)
+
+def check():
+    if inputBox_food.get() == "Egg":
+
+        if inputBox_amount.get() == "Per 100 gram": 
+            label.config(text="Kalorier: 155")
+
+        if inputBox_amount.get() == "1 stykke": 
+            label.config(text="Kalorier: 80")
+
+button = Button(window, text= "Text", command = check)  
+button.place(x=100, y=100)  
+
+window.mainloop()
